f8613ca4af06e822eeed0ea926ba912af018bdf6
Mike-7777777 <mike467901371@yahoo.com>
2021-02-25 23:52:37 +0800

    commit intro -post -bodyparse

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.render("intro");
 });
 app.post("/go", (req, res) => {
-  res.render("room", { roomId: req.body.query.room,  userName: req.body.query.name});
+  res.render("room", { roomId: req.body.room,  userName: req.body.name});
 });
 // 如果进入特定房间 则跳转到该房间
 // 此处的/:room 是一个变量
